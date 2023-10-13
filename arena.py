e88762ead6625c3d136ddd27000dd20379d5be0e
docnt <docnt@gmail.com>
2023-10-13 02:09:40 -0600

    arena4

--- arena.py
+++ arena.py
@@ -1,7 +1,18 @@
 import pyautogui
 import time
 
+#esto ejecuta algo simple
+caja = (500, 500)
+barra = (500, 500)
 
-#desde aqui comienza la automatizacion
+ #desde aqui comienza la automatizacion
 time.sleep(1)
+
+pyautogui.click(caja)
+#luego hacemos algun scroll
+pyautogui.scroll(-500)
+time.sleep(1)
+
+
+#y luego aqui otro
 
