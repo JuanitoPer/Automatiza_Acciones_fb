4bd089fb3b0e4198a52cf032385bdc29c2f9cdb6
docnt <docnt@gmail.com>
2023-10-12 22:34:30 -0600

    nuevo commit

---
+++ screen_Posicion.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time 
+# Obtener la posición actual del puntero
+
+time.sleep(5)       
+
+x, y = pyautogui.position()
+
+# Imprimir las coordenadas x e y
+print(f"Coordenadas actuales: x = {x}")
+print(f"Coordenadas actuales: y = {y}")
